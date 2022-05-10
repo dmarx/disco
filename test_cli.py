9895795f310677fc103d288cbd690a19359bad44
Chigozie Nri <chigozie@caltech.deu>
2022-05-09 19:03:36 -0500

    Allow --prompt arg in test_cli.py

--- test_cli.py
+++ test_cli.py
@@ -18,8 +18,9 @@ from modules.manager.projects.api import Api
 from modules.manager.projects.project import Project
 
 
-async def run_custom():
-    prompt = "A scenic view over a landscape of magical architecture, by David Noton and Asher Brown Durand, matte painting trending on artstation HQ."
+async def run_custom(
+    prompt="A scenic view over a landscape of magical architecture, by David Noton and Asher Brown Durand, matte painting trending on artstation HQ.",
+):
 
     project = Project(1)
     project.generators = [
@@ -48,58 +49,23 @@ async def run_custom():
                     "height": 380,
                     "ViTB32": True,
                     "ViTB16": True,
-                    #'ViTL14': False,
+                    "ViTL14": False,
                     "ViTL14_336px": False,
-                    # 'RN101': False,
-                    # 'RN50': False,
-                    #  'RN50x4': False,
+                    "RN101": False,
+                    "RN50": False,
+                    "RN50x4": False,
                     "RN50x16": False,
-                    # 'RN50x64': False,
-                    "frames_scale": 1500,  # @param{type: 'integer'}
+                    "RN50x64": False,
+                    "frames_scale": 1500,
                     "frames_skip_steps": "65%",
                     "turbo_mode": True,
                     "turbo_steps": "3",
-                    "skip_steps": 10,  # was 20
+                    "skip_steps": 10,
                     "animation_mode": "None",
                     "max_frames": 10000,
                 },
             }
         ),
-        #      SimpleNamespace(**{
-        #     "id":3
-        #     "type":3,
-        #     "settings":{
-        #     "width":512,
-        #         "height":512,
-        #         }
-        #     # ),
-        #      SimpleNamespace(**{
-        #     "id":4
-        #     "type":4,
-        #     "settings":{
-        #     "width":512,
-        # # #         "height":512,
-        # #         }
-        # })
-        # SimpleNamespace(**{
-        #   'id':0,
-        #   'type':4,
-        #   'settings':{
-        #       'prompt':prompt,
-        #       'steps':150,
-        #       'width':512,
-        #      'height':512,
-        # 'ViTB32': True,
-        # 'ViTB16': True,
-        # 'ViTL14': False, # True
-        # 'ViTL14_336px':False,
-        # 'RN101': False,
-        # 'RN50': False,
-        # 'RN50x4': False,
-        # 'RN50x16': False,
-        # 'RN50x64': False,
-        #  }
-        # })
     ]
 
     print("running project chain...")
@@ -114,14 +80,16 @@ async def run_external_project(project_id):
     chain.filename = await chain.run_project(project)
 
 
+default_prompt = "A scenic view over a landscape of magical architecture, by David Noton and Asher Brown Durand, matte painting trending on artstation HQ."
+
 parser = argparse.ArgumentParser()
 parser.add_argument("--project", type=int, default="0", help="project id")
+parser.add_argument("--prompt", type=str, default=default_prompt, help="prompt")
 args = parser.parse_args()
 
 loop = asyncio.new_event_loop()
 asyncio.set_event_loop(loop)
 if args.project > 0:
-    # XXX: Project.load_data() needs to be fixed so it produces Project.generators with the right data types (generator.settings needs to be a dict, not a SimpleNamespace
     responses = loop.run_until_complete(run_external_project(args.project))
 else:
-    responses = loop.run_until_complete(run_custom())
+    responses = loop.run_until_complete(run_custom(args.prompt))
